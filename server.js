4c6b1c104213389be758837e3c8106e1a8b0e7cf
Troy Whiteley <troy.whiteley@demandmedia.com>
2011-03-07 15:24:07 -0800

    testing a change

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ var server = ws.createServer();
 server.addListener("connection", function(connection){
 	sys.debug('socket connection started');
 	connection.addListener("message", function(msg){
-		server.send(msg);
+		server.broadcast(msg);
 	});
 });
 
